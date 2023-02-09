a4d07cbe5c427aa3a5724f596d566f847dcbf7dd
aaronlovell7 <aaronlovellmu42@gmail.com>
2023-02-08 20:05:09 -0500

    adding reading in txt file with urls

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 // compile with: tsc main.ts
 // execute with: node ./main.js
 // if there is an error: "cannot find the name 'process'", run: 'npm i --save-dev @types/node'
+import * as fs from 'fs';
+
 import { Module } from "./Module";
 import { Metric } from "./Metric";
 import { RampUp } from "./RampUp";
@@ -8,6 +10,8 @@ import { Correctness } from "./Correctness";
 import { BusFactor } from "./BusFactor";
 import { Responsiveness } from "./Responsiveness";
 import { Licensing } from "./Licensing"
+import { exit } from "process";
+import { fileURLToPath } from 'url';
 
 // example: array of URLs --> assuming we read in the input file as strings
 let urlArray : string[] = [ "www.mockurl1.com", "www.pretendurl.com", "www.testingurl.com" ];
@@ -17,6 +21,14 @@ let moduleArray : Module[] = [];
 const argv = process.argv.slice(2)
 
 let flag = argv[0];
+
+console.log(urlArray);
+
+// how to read file in as array
+//const urlFile = fs.readFileSync('./sample_url.txt', 'utf-8');   // tested using sample url file
+const urlFile = fs.readFileSync(flag, 'utf-8');                 // this uses the flag passed through in 'run' for the destination of the file    
+urlArray = urlFile.split('\n');
+console.log(urlArray);
 
 // then we create an array of Module objects with the urls
 for (var url in urlArray)
