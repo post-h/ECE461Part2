908d5e5b3ccbc2f62de59efcff42aa224b8c1be4
Jack Stavrakos <jstavrak@purdue.edu>
2023-02-12 16:16:03 -0500

    Licensing changed to use graphql

--- Licensing.ts
+++ Licensing.ts
@@ -31,7 +31,7 @@ async function calc(_owner: string, _repo: string) : Promise<number>
     const response = await fetch("https://api.github.com/graphql", {
         method: "POST",
     headers: {
-        Authorization: `Token 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`,
+        Authorization: `Token ${process.env.GITHUB_TOKEN}`,
     },
     body: JSON.stringify({ query: gql_query }),
     });
