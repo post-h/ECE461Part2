27217cc81cd24337aaeb641b6a082f3b77136216
William Jorge <wjorgebe@purdue.edu>
2023-04-30 16:44:38 -0400

    Fixed erros for package update

--- api_test.py
+++ api_test.py
@@ -1,7 +1,10 @@
 import requests
 
-url = 'http://127.0.0.1:5000/package/underscore'
-response = requests.delete(url)
+url = 'http://127.0.0.1:5000/package/lodash'
+data = {"metadata": {"Name": "string", "Version": "4.17.21", "ID": "string"},
+ "data": {"Content": "string", "URL": "string", "JSProgram": "string"}}
+
+response = requests.put(url, json=data)
 
 if response.status_code == 200:
     print(response.json())
